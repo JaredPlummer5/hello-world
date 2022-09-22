58d9f839bd842c95f2c11cc82c7df6e3d8c78fdc
Jared Plummer <jaredplummer5@icloud.com>
2022-09-22 09:13:16 -0500

    I change the images to real ones to make my website look better. I also added a CSS and JavaScript fil to style my website and create pop-ups so the user of my webpage at interact with it dynamicly

---
+++ app.js
@@ -0,0 +1,67 @@
+alert("Hi welcome to Coffee Plaza!!!");
+let favoriteCoffee = prompt("What is your favorite type of coffee?");
+document.write(favoriteCoffee)
+//const coffeeGoodchoice = ["Latte", "Mocha", "Cappucino", "Black", "Espresso"];
+if (favoriteCoffee == "Latte" ){
+    alert("Good Choice");
+}
+else if (favoriteCoffee == "Mocha" ){
+    alert("Good Choice");
+}
+else if (favoriteCoffee == "Cappucino" ){
+    alert("Good Choice");
+}
+else if (favoriteCoffee == "Black" ){
+   alert("I'm not judging because I know you're tired.");
+}
+else if (favoriteCoffee == "Espresso" ){
+    alert("I'm not judging because I know you're tired.");
+ }
+else if (favoriteCoffee == "Galao"){
+    alert("Would you like a little Coffee with your foam?")
+} 
+else if (favoriteCoffee == "Galao"){
+    alert("Would you like a little Coffee with your foam?")
+} 
+else if (favoriteCoffee == "Galao"){
+    alert("Would you like a little Coffee with your foam?")
+} 
+else if (favoriteCoffee == "Macchiato"){
+    alert("Would you like a little Coffee with your foam?")
+} 
+else if (favoriteCoffee == "Cortado"){
+    alert("Would you like a little Coffee with your milk?")
+} 
+else if (favoriteCoffee == "Flat White"){
+    alert("Would you like a little Coffee with your milk?")
+} 
+else if (favoriteCoffee == "Cafe Au Lait"){
+    alert("Would you like a little Coffee with your milk?")
+} 
+
+else if (favoriteCoffee == "Red Eye"){
+    alert("Seek help immediatly")
+} 
+else if (favoriteCoffee == "Doppio"){
+    alert("Seek help immediatly")
+} 
+else if (favoriteCoffee == "Lungo"){
+    alert("Seek help immediatly")
+} 
+else if (favoriteCoffee == "Ristretto"){
+    alert("You have way too much energy already if that's all the caffine you need.")
+} 
+else if (favoriteCoffee == "Americano"){
+    alert("You have way too much energy already if that's all the caffine you need.")
+} 
+else if (favoriteCoffee == "Affogato"){
+    alert("You're actually really cool.")
+} 
+else if (favoriteCoffee == "Irish"){
+    alert("You're actually really cool.")
+} 
+else {
+    alert("This doesn't exsist because I said so or you typed something incorrectly. Look at the picture below and refresh the page and type one of the options.")
+}
+
+console.log(favoriteCoffee);
